e266e13b0f90a574a14db7c309000676faf03017
Bryan <twbikeman@gmail.com>
2021-04-18 21:06:09 +0800

    finally soloved

--- SAES.java
+++ SAES.java
@@ -1,14 +1,3 @@
-// import java.util.HashMap;
-
-
-// class Pair {
-//     private int x, y;
-//     public Pair(int x, int y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-// }
-
 
 
 public class SAES {
@@ -18,7 +7,7 @@ public class SAES {
                 System.out.printf("%s\n", ("00000000").substring(output.length()) + output);
     }
 
-    public int[] sBox = {0xa, 0x4, 0xa, 0xb, 
+    public int[] sBox = {0x9, 0x4, 0xa, 0xb, 
                       0xd, 0x1, 0x8, 0x5,
                       0x6, 0x2, 0x0, 0x3,
                       0xc, 0xe, 0xf, 0x7};
@@ -70,12 +59,12 @@ public class SAES {
         int w0 = x[0];
         int w1 = x[1];
         int w2 = rotnib(w1); // rotnib
-        w2= (sBox[w2 >> 4] << 4) | (sBox[w2 << 28 >>> 28]); // subnib
+        w2 = (sBox[w2 >> 4] << 4) | (sBox[w2 & 0x0F]); // subnib
         w2 = w0 ^ rcon[0] ^ w2;
         
         int w3 = w2 ^ w1;
         int w4 = rotnib(w3);
-        w4 = (sBox[w4 >> 4] << 4) | (sBox[w4 << 28 >>> 28]); // subnib
+        w4 = (sBox[w4 >> 4] << 4) | (sBox[w4 & 0x0F]); // subnib
         w4 = w2 ^ rcon[1] ^ w4;
         int w5 = w4 ^ w3;
 
@@ -84,70 +73,17 @@ public class SAES {
         return y;
     }
 
-    public void addKey(int[] x, int key) {
-        x[0] =  x[0] ^ (key >>> 12);
-        x[1] = x[1] ^ (key >>> 4 << 28 >>> 28);
-        x[2] = x[2] ^ (key >>> 8 << 28 >>> 28);
-        x[3] = x[3] ^ (key << 28 >>> 28);
+    public void addKey(int[] x, int[] key) {
+        x[0] =  x[0] ^ (key[0] >> 4);
+        x[1] =  x[1] ^ (key[1] >> 4);
+        x[2] =  x[2] ^ (key[0] & 0x0F);
+        x[3] =  x[3] ^ (key[1] & 0x0F);
+
+       
     }
 
     public static void main(String[] args) {
-        // String s = Integer.toBinaryString(10);
-        // System.out.println(s);
-
-        // int x1 = 0b1010;
-        // String s1 = Integer.toString(x1, 2);
-        // System.out.println(s1);
-
-        // String x = "A";
-        // int s = Integer.parseInt(x, 16);
-        // int s = Integer.valueOf("1010", 2);
-        // System.out.println(s);
-
-        // int sBox[] = {0xa, 0x4, 0xa, 0xb, 
-        //               0xd, 0x1, 0x8, 0x5,
-        //               0x6, 0x2, 0x0, 0x3,
-        //               0xc, 0xe, 0xf, 0x7};
-
-        // int x = sBox[0x1];
-        // System.out.println(Integer.toString(x, 16));
-
-        // String plaintext = "0110111101101011";
-        // int s00 = Integer.valueOf(plaintext.substring(0,4),2);
-        // int s10 = Integer.valueOf(plaintext.substring(4,8),2);
-        // int s01 = Integer.valueOf(plaintext.substring(8,12),2);
-        // int s11 = Integer.valueOf(plaintext.substring(12,16),2);
-
-        // System.out.println(Integer.toString(s00, 2));
-        // System.out.println(Integer.toString(s10, 2));
-        // System.out.println(Integer.toString(s01, 2));
-        // System.out.println(Integer.toString(s11, 2));
-
-        // String key = "1010011100111011";
-        // int w0 = Integer.valueOf(key.substring(0,8),2);
-        // int w1 = Integer.valueOf(key.substring(8,16),2);
-
-        // System.out.println(Integer.toString(w0, 2));
-        // System.out.println(Integer.toString(w1, 2));
-
-
-        // Map<Integer, Map<Integer, Integer> > mixColmn;
-        // HashMap<Pair, Integer> MixColmn = new HashMap<Pair, Integer>();
-        // Pair p1 = new Pair(1, 1);
-        // MixColmn.put(p1, 2);
-        // int[][] MixColumn = {{0x2, 0x4, 0x6, 0x8, 0xa, 0xc, 0xe, 0x3, 0x1, 0x7, 0x5, 0xb, 0x9, 0xf, 0xd},
-        //  {0x4, 0x8, 0xc, 0x3, 0x7, 0xb, 0xf,0x6, 0x2, 0xe, 0xa, 0x5, 0x1, 0xd, 0x9},
-        // {0x9, 0x1, 0x8, 0x2, 0xb, 0x3, 0xa, 0x4, 0xd, 0x5, 0xc, 0x6, 0xf, 0x7, 0xe}};
-
-
-        // System.out.println(Integer.toString(MixColumn[0x2 / 2 - 1][0xf - 1], 16));
-
-
-        // String s = "AABBCCDD";
-
-        // System.out.printf("%s\n", ("00000000").substring(s.length()) + s);
-        
-
+  
         SAES saes = new SAES();
 
 
@@ -173,6 +109,13 @@ public class SAES {
         int[] wExpand = saes.keyExpansion(w);
         for (int i: wExpand)
             saes.print(i);
+        int w2 = wExpand[2];
+        int w3 = wExpand[3];
+        int w4 = wExpand[4];
+        int w5 = wExpand[5];
+        int[] key0 = {w0, w1};
+        int[] key1 = {w2, w3};
+        int[] key2 = {w4, w5};
 
 
         System.out.println("----");
@@ -180,51 +123,24 @@ public class SAES {
 
 
 
-        // //round 0
-        saes.addKey(s, (w0 << 8 | w1));
+        // // //round 0
+        saes.addKey(s, key0);
        
-        // //round 1
+        // // // //round 1
         saes.nibSub(s);
         saes.shiftRow(s);
         saes.mixColumn(s);
-        saes.addKey(s, (wExpand[2] << 8 | wExpand[3]));
-        // //round 2
+        saes.addKey(s, key1);
+        // // // // //round 2
         saes.nibSub(s);
         saes.shiftRow(s);
-        saes.addKey(s, (wExpand[4] << 8 | wExpand[5])); 
+        saes.addKey(s, key2); 
 
 
         for (int i: s)
         saes.print(i);
         
-        
-            // int[] rcon = {0b10000000, 0b00110000};
-
-     
-        // String input = "2D55";
-        // int w0 = Integer.valueOf(input.substring(0, 2), 16);
-        // int w1 = Integer.valueOf(input.substring(2, 4), 16);
-        // int[] x = {w0, w1};
-        // int[] w = saes.keyExpansion(x);
-            
-        // for (int i : w)
-        // saes.print(i);
-
-        // int w2 = saes.rotnib(w1); // rotnib
-        // w2= (saes.sBox[w2 >> 4] << 4) | (saes.sBox[w2 << 28 >>> 28]); // subnib
-        // w2 = w0 ^ rcon[0] ^ w2;
-        // String w2String = Integer.toString(w2,2);
-
-
-        //rotnib
-
-
-        // System.out.printf("%s\n", ("00000000").substring(w0String.length()) + w0String);
-        
-        // System.out.printf("%s\n", ("00000000").substring(w2String.length()) + w2String);
-
-
-
+       
 
     }   
 
